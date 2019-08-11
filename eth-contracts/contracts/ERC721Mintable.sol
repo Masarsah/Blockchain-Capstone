457e9d1d3e7aeea8a223df854aafc416d497bbab
Masarah Fallath <masarah@MacBook-Air-alkhas-b-Masarah.local>
2019-08-12 00:59:58 +0300

    TODO 2 ERC721MINABLE

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -150,27 +150,30 @@ contract ERC721 is Pausable, ERC165 {
     function balanceOf(address owner) public view returns (uint256) {
         // TODO return the token balance of given address
         // TIP: remember the functions to use for Counters. you can refresh yourself with the link above
+        return _ownedTokensCount[owner].current();
     }
 
     function ownerOf(uint256 tokenId) public view returns (address) {
         // TODO return the owner of the given tokenId
+        return _tokenOwner[tokenId];
     }
 
 //    @dev Approves another address to transfer the given token ID
     function approve(address to, uint256 tokenId) public {
         
         // TODO require the given address to not be the owner of the tokenId
-
+       require(to != _tokenOwner[tokenId]);
         // TODO require the msg sender to be the owner of the contract or isApprovedForAll() to be true
-
+       require(msg.sender == _tokenOwner[tokenId] || isApprovedForAll(_tokenOwner[tokenId], msg.sender));
         // TODO add 'to' address to token approvals
-
+       _tokenApprovals[tokenId] = to;
         // TODO emit Approval Event
-
+       emit Approval(_tokenOwner[tokenId], to, tokenId);
     }
 
     function getApproved(uint256 tokenId) public view returns (address) {
         // TODO return token approval if it exists
+         return _tokenApprovals[tokenId];
     }
 
     /**
